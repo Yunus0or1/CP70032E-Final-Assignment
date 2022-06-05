d562214e7da9991aff2e6c179a855359d2352d61
Borys Kubisty <dropcmd@gmail.com>
2022-06-05 01:09:05 +0100

    basic dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { Typography, Container, Box, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectUser } from "../state/authSlice";
 
 export const Dashboard = () => {
+  const navigate = useNavigate();
+  const user = useSelector(selectUser);
+
+  console.log(user);
+
   return (
     <>
       <Container
@@ -18,12 +26,36 @@ export const Dashboard = () => {
           sx={{
             display: "flex",
             flexDirection: "column",
+            alignItems: "center",
             gap: 3,
           }}
         >
-          <Typography variant="h4">Dashboard</Typography>
-          <Button variant="contained">Event List</Button>
-          <Button variant="contained">Booking List</Button>
+          <Typography variant="h4">WestTheatre Dashboard</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography>
+              Welcome,{" "}
+              <b>
+                {user.firstName} {user.lastName}
+              </b>
+            </Typography>
+            <Typography>
+              You are a <b>{user.userType}</b>
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", gap: 2 }}>
+            <Button variant="contained" onClick={() => navigate("/events")}>
+              Event List
+            </Button>
+            <Button variant="contained" onClick={() => navigate("/bookings")}>
+              Booking List
+            </Button>
+          </Box>
         </Box>
       </Container>
     </>
